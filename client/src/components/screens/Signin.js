c5bea7fbaa13c6c246ae1f76e385f2e7d6a1c377
Moniruzzaman <monir150227@gmail.com>
2021-01-05 18:11:46 +0600

    Prepare for deployment

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -54,7 +54,7 @@ const Signin = () => {
         M.toast({ html: message, classes: "success" });
 
         setSignInUser({ ...SIGNIN_USER_INITIAL_STATE });
-        history.push("/home");
+        history.push("/");
       } else {
         setErrorMsg({ ...errors });
       }
