00c84cb29d2392a64f961e00fe01e76872b530b8
Moniruzzaman <monir150227@gmail.com>
2021-01-05 18:05:02 +0600

    Prepare for deployment

--- client/src/constants/apiUrl.js
+++ client/src/constants/apiUrl.js
@@ -1,15 +1,15 @@
-const PORT = process.env.REACT_APP_PORT || "http://localhost:5000";
+// const PORT = process.env.REACT_APP_PORT || "http://localhost:5000";
 
-export const USER_SIGNUP_POST_URL = `${PORT}/api/users/signup`;
-export const USER_SIGNIN_POST_URL = `${PORT}/api/users/signin`;
-export const USER_PASSWORD_RESET_POST_URL = `${PORT}/api/users/reset-password`;
-export const USER_NEW_PASSWORD_RESET_POST_URL = `${PORT}/api/users/new-password`;
+export const USER_SIGNUP_POST_URL = `/api/users/signup`;
+export const USER_SIGNIN_POST_URL = `/api/users/signin`;
+export const USER_PASSWORD_RESET_POST_URL = `/api/users/reset-password`;
+export const USER_NEW_PASSWORD_RESET_POST_URL = `/api/users/new-password`;
 
-export const POST_POST_URL = `${PORT}/api/posts`;
-export const POST_GET_URL = `${PORT}/api/posts`;
-export const POST_GET_MYPOST_URL = `${PORT}/api/posts/mypost`;
-export const POST_UPDATE_LIKE_UNLIKE_URL = `${PORT}/api/posts/likeunlike`;
-export const POST_UPDATE_COMMENT_URL = `${PORT}/api/posts/comment`;
+export const POST_POST_URL = `/api/posts`;
+export const POST_GET_URL = `/api/posts`;
+export const POST_GET_MYPOST_URL = `/api/posts/mypost`;
+export const POST_UPDATE_LIKE_UNLIKE_URL = `/api/posts/likeunlike`;
+export const POST_UPDATE_COMMENT_URL = `/api/posts/comment`;
 
 // Cloudnary image upload apiUrl
 
